f6694df31f0912225239226c849abf325db64d14
thinkdifferentvasubro <thinkdifferentvasubro@gmail.com>
2026-05-21 02:17:41 +0530

    update

--- src/serving/inference.py
+++ src/serving/inference.py
@@ -5,11 +5,7 @@ import mlflow
 
 #loading model
 current_dir = os.path.dirname(os.path.abspath(__file__))
-try:
-    model = mlflow.sklearn.load_model(
-    os.path.join(current_dir, "model"))
-except Exception as e:
-    print(f"{e} model not found")
+model = mlflow.sklearn.load_model(os.path.join(current_dir, "model"))
 
 def predictions(Data):
     try:
